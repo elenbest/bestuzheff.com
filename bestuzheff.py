745a91495aa06d3461829e418188bbc37700ef17
bestuzheff <sergej.bestuzhev@gmail.com>
2021-08-21 10:42:37 +0300

    Поменял порт на 80

--- bestuzheff.py
+++ bestuzheff.py
@@ -1,13 +1,13 @@
 from flask import Flask
+from waitress import serve
 app = Flask(__name__)
-
-index_file = open('index.html', 'r')
 
 
 @app.route('/')
 def hello_world():
+    index_file = open('index.html', 'r')
     return index_file.read()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    serve(app, host='0.0.0.0', port=80)
