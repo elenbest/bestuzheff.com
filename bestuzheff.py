b5ed738f53d4a75bded3d83f61297a679951e257
bestuzheff <sergej.bestuzhev@gmail.com>
2021-08-21 09:49:36 +0300

    My site on Python

--- bestuzheff.py
+++ bestuzheff.py
@@ -1,1 +1,13 @@
-print("test!")
+from flask import Flask
+app = Flask(__name__)
+
+index_file = open('index.html', 'r')
+
+
+@app.route('/')
+def hello_world():
+    return index_file.read()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
